3ee53cd588934f3d87aba8f8053bfdab23a8d709
student <student@students-iMac-59.local>
2017-05-09 14:55:41 -0700

    first commit on new branch

--- app/src/main/java/io/iaso/iaso/UserAccountHome/UserAccountHome.java
+++ app/src/main/java/io/iaso/iaso/UserAccountHome/UserAccountHome.java
@@ -1,6 +1,7 @@
 package io.iaso.iaso.UserAccountHome;
 
 import android.accounts.AccountManager;
+import android.app.AlarmManager;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -9,6 +10,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
@@ -18,14 +20,20 @@ import android.view.View;
 import android.widget.Button;
 
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
 import io.iaso.iaso.AccountSettingsActivity;
 import io.iaso.iaso.ApplicationInstance;
 import io.iaso.iaso.MedicineDetailActivity;
+import io.iaso.iaso.NotificationPublisher.NotificationPublisher;
 import io.iaso.iaso.NotificationSettingsActivity;
 import io.iaso.iaso.Prescription.AddPrescriptionActivity;
 import io.iaso.iaso.R;
 import io.iaso.iaso.adapter.RecyclerViewAdapter;
 import io.iaso.iaso.auth.*;
+import io.iaso.iaso.core.model.Medicine;
 import io.iaso.iaso.core.model.MedicineResponse;
 import io.iaso.iaso.network.async.MedicineCallbackListener;
 import io.iaso.iaso.network.async.MedicineListTask;
@@ -103,7 +111,7 @@ public class UserAccountHome extends AppCompatActivity {
                 startActivity(toNotifSettings);
             }
         });
-
+    /*
         String contextText =  "Stuff";//"Take " + medicineItems.get(0).getMed_name() + ", " + medicineItems.get(0).getNextDose();
 
         Intent resIntent = new Intent(this, MedicineDetailActivity.class);
@@ -121,12 +129,15 @@ public class UserAccountHome extends AppCompatActivity {
         NotificationManager notiManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         notiManager.notify(0, notification);
+        */
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         String magicalTokenOfDestiny = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjU5MDYzYjRhMDMzZmI1MjM2NGIyNWJiZCIsImlhdCI6MTQ5MzU4MTA5M30.wv9cNaZf1HAjj4Pt8VZUHj-MulM9ee1CEWVu-kKZB0I";
+
+        scheduleNotification(this, "2030", 1);
 
         UserAccountRecycler = (RecyclerView) findViewById(R.id.recycler_view);
         UserAccountlayoutManager = new LinearLayoutManager(getBaseContext());
@@ -145,6 +156,72 @@ public class UserAccountHome extends AppCompatActivity {
         task.setMedicineCallbackListener(medicineCallbackListener);
         task.execute(magicalTokenOfDestiny); //magic token??
     }
+
+    public void scheduleNotification(Context context, String delay, int notificationId) {
+
+        String contentText =  "Stuff";//"Take " + medicineItems.get(0).getMed_name() + ", " + medicineItems.get(0).getNextDose();
+        Intent resIntent = new Intent(context, UserAccountHome.class); // intent to take you to once you click on the notification, I think.
+        PendingIntent pIntent = PendingIntent.getActivity(context, notificationId, resIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder nBuilder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.iaso_bottle)
+                .setContentTitle("Reminder, Take your Meds")
+                .setContentText(contentText)
+                .setContentIntent(pIntent);
+        //.setAutoCancel(true); // sets up params for notification
+
+        Notification notification = nBuilder.build(); // builds notification
+
+        Intent notificationIntent = new Intent(context, NotificationPublisher.class); // potentially misleading variable name.. not actually the intent for the notification
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION_ID, notificationId);
+        notificationIntent.putExtra(NotificationPublisher.NOTIFICATION, notification);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(context, notificationId, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //calendar.add(Calendar.MINUTE, shpref.getInt("timeoutint", 30));
+
+        long notificationDelay = hoursToMillis(delay);
+        //long notificationDelay = hoursToMillis(delay); I'm Crazy lol
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.set(AlarmManager.RTC_WAKEUP, 0 , pendingIntent);
+
+    }
+
+    public ArrayList parseDosageTimes(Medicine medicine) {
+        String dosageTimes = medicine.getDosage_times(); // times each dose gets taken
+        Integer doseFrequency = medicine.getDoses_per_day(); // amount of doses per day
+        ArrayList<String> times = new ArrayList<>();
+        String s = " ";
+        for (int i = 0; i < dosageTimes.length(); i++)
+        {
+            s += dosageTimes.charAt(i);
+
+            if (i % 4 == 3)
+            {
+                times.add(s);
+                s = " ";
+            }
+        }
+
+        return times;
+    }
+
+    public long hoursToMillis(String time) {
+        //long intTime = Integer.parseInt(time);
+        Log.d("Everything is fine", time);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis()); // may not use at all
+        //calendar.setTime(); // may use for later
+        long hours = calendar.get(Calendar.HOUR_OF_DAY);
+        long minutes = calendar.get(Calendar.MINUTE);
+        long delayHours = Integer.parseInt(time.substring(0, 2)) - hours;
+        long delayMinutes = Integer.parseInt(time.substring(2, 4)) - minutes;
+
+        long difference = TimeUnit.HOURS.toMillis(delayHours) + TimeUnit.MINUTES.toMillis(delayMinutes);
+        Log.d("current diff in millis", String.valueOf(difference));
+        return difference;
+    }
+
+
+
 
 }
 
